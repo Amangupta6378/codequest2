9a0efd9835afdef538fd94dadc385c5d334abf49
Shital Sinha <shitalsinha18@gmail.com>
2024-04-23 12:54:19 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      
+    
+      <Sidebar/>
     </div>
+
   );
 }
 
