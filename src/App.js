72402d9352f4a2fb996ce4d11b4087d1853da0c8
vikas <shahv7411@gmail.com>
2024-04-23 13:01:47 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Pofile';
 
 function App() {
   return (
     <div className="App">
-      
+      <Profile/>
     </div>
   );
 }
---
+++ src/components/Pofile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+        <div className='flex w-100 justify-center mt-10'>
+            <div className='w-20 h-20 rounded-full bg-gray-400 '><img></img></div>
+            <div className='ms-10 font-medium font-sarif'>
+              <p>Aman Gupta</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
